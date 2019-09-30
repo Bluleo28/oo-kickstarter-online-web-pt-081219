ee0523f5fc158dd4c7a5d67145a732738f4ac4c5
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-30 03:21:30 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ def initialize(name)
   @backed_projects = []
 end 
 def back_project(project)
-  backed_projects = project
-  backed_projects << project  
+  backed_projects << project
+  project.backers << self   
   end
 end
